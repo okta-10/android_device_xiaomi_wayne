0d87e4018ccc68796d6e823fedf7e2935d495771
Oktapra Amtono <oktapra.amtono@gmail.com>
2021-01-04 14:31:08 +0700

    wayne: Build Deskclock
    Track deskclock from los
    
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- nad_wayne.mk
+++ nad_wayne.mk
@@ -37,6 +37,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Pixel-charging
 USE_PIXEL_CHARGING := true
 
+# Package Apps
+PRODUCT_PACKAGES += \
+    DeskClock
+
 # Build Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="wayne-user 9 PKQ1.180904.001 V10.3.4.0.PDCCNXM release-keys"
