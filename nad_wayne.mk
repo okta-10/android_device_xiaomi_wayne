d222bbfbb455da27e4db3b1581c89d397fcbda0f
Oktapra Amtono <oktapra.amtono@gmail.com>
2021-06-06 19:04:25 +0700

    Revert "wayne: add NusantaraParts & papers"
    we have build Unofficial
    
    This reverts commit 262295b06f1a82bb0d6493ddf00c91e2eadc9e19.

--- nad_wayne.mk
+++ nad_wayne.mk
@@ -27,8 +27,6 @@ $(call inherit-product, device/xiaomi/wayne/device.mk)
 # Inherit Nusantara product configuration
 $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
-$(call inherit-product-if-exists, packages/apps/NusantaraParts/nadproject.mk)
-
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
